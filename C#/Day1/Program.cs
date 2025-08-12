cb074fc741d8c41458e2d37feef1947846a6ae5a
Sara Reda <113102407+sarareda-787@users.noreply.github.com>
2025-08-12 22:06:42 +0300

    Task2

---
+++ C#/Day1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('hello');
+        }
+            
+    }
+}
